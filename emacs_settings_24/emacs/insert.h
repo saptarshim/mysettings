004b606bcd6e08dce8876da083fd5cd3e0f51fbc
Saptarshi Mondal <Saptarshi.Mondal@gmail.com>
2012-08-13 01:19:39 -0700

    Updating the auto insert files

--- emacs_settings_24/emacs/insert.h
+++ emacs_settings_24/emacs/insert.h
@@ -1,13 +1,11 @@
-/*
-Copyright Khudro Udyog (C) 2011-2015 All Right Reserverd
-File Name:
-Author(s):
-Purpose:
-Date Created:
-Date Updated:
+/**
+ *Copyright Khudro Udyog (C) 2011-2015 All Right Reserved
+ *File Name: @@@ 
+ *Author(s): Saptarshi Mondal
+ *email: saptarshi.mondal@gmail.com
+ *Purpose:
+ *Date Created: DDDD
+ *Date Updated:
 */
 
-#if !defined(@@@)
-#define @@@
 
-#endif
